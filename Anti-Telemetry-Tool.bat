6e1d36e004aef6a1ebc4b1127170520fcbd49834
if3xist <mf666@protonmail.ch>
2017-03-08 20:52:07 -0500

    Retired No-Cortana Updated ATT
    Retired No-Cortana.bat. Updated the method for inserting the blocked
    domains into the hosts file.

--- Anti-Telemetry-Tool.bat
+++ Anti-Telemetry-Tool.bat
@@ -1,9 +1,9 @@
-:::::::::::::::::::::::::::::::::::::::::::
-:: This tool was made on 3/6/17 by mf666 ::
-:: Please use about function in script.  ::
-::                                       ::
-:: Contact: mf666@protonmail.ch          ::
-:::::::::::::::::::::::::::::::::::::::::::
+::::::::::::::::::::::::::::::::::::::::::::::::
+::  This tool was updated on 3/8/17 by mf666  ::
+::Read the about section for more information!::
+::                                            ::
+::       Contact: mf666@protonmail.ch         ::
+::::::::::::::::::::::::::::::::::::::::::::::::
 
 @echo off
 set TTL=title mf666's Anti-Telemetry Tool -
@@ -54,119 +54,121 @@ cls
 %TTL% ANONYMIZING YOUR MACHINE, PLEASE WAIT...
 
 echo Now BLOCKING all Telemetry hosts...
-echo #BEGIN MF666 ANTI-TELEMETRY >> %HOSTS%
-echo 0.0.0.0	choice.microsoft.com >> %HOSTS%
-echo 0.0.0.0	choice.microsoft.com.nstac.net >> %HOSTS%
-echo 0.0.0.0	df.telemetry.microsoft.com >> %HOSTS%
-echo 0.0.0.0	oca.telemetry.microsoft.com >> %HOSTS%
-echo 0.0.0.0	oca.telemetry.microsoft.com.nsatc.net >> %HOSTS%
-echo 0.0.0.0	redir.metaservices.microsoft.com >> %HOSTS%
-echo 0.0.0.0	reports.wes.df.telemetry.microsoft.com >> %HOSTS%
-echo 0.0.0.0	services.wes.df.telemetry.microsoft.com >> %HOSTS%
-echo 0.0.0.0	settings-sandbox.data.microsoft.com >> %HOSTS%
-echo 0.0.0.0	settings-win.data.microsoft.com >> %HOSTS%
-echo 0.0.0.0	sqm.df.telemetry.microsoft.com >> %HOSTS%
-echo 0.0.0.0	sqm.telemetry.microsoft.com >> %HOSTS%
-echo 0.0.0.0	sqm.telemetry.microsoft.com.nsatc.net >> %HOSTS%
-echo 0.0.0.0	telecommand.telemetry.microsoft.com >> %HOSTS%
-echo 0.0.0.0	telecommand.telemetry.microsoft.com.nsatc.net >> %HOSTS%
-echo 0.0.0.0	telemetry.appex.bing.net >> %HOSTS%
-echo 0.0.0.0	telemetry.microsoft.com >> %HOSTS%
-echo 0.0.0.0	telemetry.urs.microsoft.com >> %HOSTS%
-echo 0.0.0.0	vortex-sandbox.data.microsoft.com >> %HOSTS%
-echo 0.0.0.0	vortex-win.data.microsoft.com >> %HOSTS%
-echo 0.0.0.0	vortex.data.microsoft.com >> %HOSTS%
-echo 0.0.0.0	watson.telemetry.microsoft.com >> %HOSTS%
-echo 0.0.0.0	watson.telemetry.microsoft.com.nsatc.net >> %HOSTS%
-echo 0.0.0.0	watson.ppe.telemetry.microsoft.com >> %HOSTS%
-echo 0.0.0.0	wes.df.telemetry.microsoft.com >> %HOSTS%
-echo 0.0.0.0	vortex-bn2.metron.live.com.nsatc.net >> %HOSTS%
-echo 0.0.0.0	vortex-cy2.metron.live.com.nsatc.net >> %HOSTS%
-echo 0.0.0.0	watson.live.com >> %HOSTS%
-echo 0.0.0.0	watson.microsoft.com >> %HOSTS%
-echo 0.0.0.0	feedback.search.microsoft.com >> %HOSTS%
-echo 0.0.0.0	feedback.windows.com >> %HOSTS%
-echo 0.0.0.0	corp.sts.microsoft.com >> %HOSTS%
-echo 0.0.0.0	diagnostics.support.microsoft.com >> %HOSTS%
-echo 0.0.0.0	i1.services.social.microsoft.com >> %HOSTS%
-echo 0.0.0.0	i1.services.social.microsoft.com.nsatc.net >> %HOSTS%
-echo 0.0.0.0	vortex-bn2.metron.live.com.nsatc.net >> %HOSTS%
-echo 0.0.0.0	vortex-cy2.metron.live.com.nsatc.net >> %HOSTS%
-echo 0.0.0.0	ca.telemetry.microsoft.com >> %HOSTS%
-echo 0.0.0.0	cache.datamart.windows.com >> %HOSTS%
-echo 0.0.0.0	spynet2.microsoft.com >> %HOSTS%
-echo 0.0.0.0	spynetalt.microsoft.com >> %HOSTS%
-echo 0.0.0.0	a.ads1.msn.com >> %HOSTS%
-echo 0.0.0.0	a.ads2.msads.net >> %HOSTS%
-echo 0.0.0.0	a.ads2.msn.com >> %HOSTS%
-echo 0.0.0.0	a.rad.msn.com >> %HOSTS%
-echo 0.0.0.0	a-0001.a-msedge.net >> %HOSTS%
-echo 0.0.0.0	a-0002.a-msedge.net >> %HOSTS%
-echo 0.0.0.0	a-0003.a-msedge.net >> %HOSTS%
-echo 0.0.0.0	a-0004.a-msedge.net >> %HOSTS%
-echo 0.0.0.0	a-0005.a-msedge.net >> %HOSTS%
-echo 0.0.0.0	a-0006.a-msedge.net >> %HOSTS%
-echo 0.0.0.0	a-0007.a-msedge.net >> %HOSTS%
-echo 0.0.0.0	a-0008.a-msedge.net >> %HOSTS%
-echo 0.0.0.0	a-0009.a-msedge.net >> %HOSTS%
-echo 0.0.0.0	ac3.msn.com >> %HOSTS%
-echo 0.0.0.0	ad.doubleclick.net >> %HOSTS%
-echo 0.0.0.0	adnexus.net >> %HOSTS%
-echo 0.0.0.0	adnxs.com >> %HOSTS%
-echo 0.0.0.0	ads.msn.com >> %HOSTS%
-echo 0.0.0.0	ads1.msads.net >> %HOSTS%
-echo 0.0.0.0	ads1.msn.com >> %HOSTS%
-echo 0.0.0.0	aidps.atdmt.com >> %HOSTS%
-echo 0.0.0.0	aka-cdn-ns.adtech.de >> %HOSTS%
-echo 0.0.0.0	a-msedge.net >> %HOSTS%
-echo 0.0.0.0	apps.skype.com >> %HOSTS%
-echo 0.0.0.0	az361816.vo.msecnd.net >> %HOSTS%
-echo 0.0.0.0	az512334.vo.msecnd.net >> %HOSTS%
-echo 0.0.0.0	b.ads1.msn.com >> %HOSTS%
-echo 0.0.0.0	b.ads2.msads.net >> %HOSTS%
-echo 0.0.0.0	b.rad.msn.com >> %HOSTS%
-echo 0.0.0.0	bs.serving-sys.com >> %HOSTS%
-echo 0.0.0.0	c.atdmt.com >> %HOSTS%
-echo 0.0.0.0	c.msn.com >> %HOSTS%
-echo 0.0.0.0	cdn.atdmt.com >> %HOSTS%
-echo 0.0.0.0	cds26.ams9.msecn.net >> %HOSTS%
-echo 0.0.0.0	compatexchange.cloudapp.net >> %HOSTS%
-echo 0.0.0.0	corpext.msitadfs.glbdns2.microsoft.com >> %HOSTS%
-echo 0.0.0.0	cs1.wpc.v0cdn.net >> %HOSTS%
-echo 0.0.0.0	db3aqu.atdmt.com >> %HOSTS%
-echo 0.0.0.0	ec.atdmt.com >> %HOSTS%
-echo 0.0.0.0	fe2.update.microsoft.com.akadns.net >> %HOSTS%
-echo 0.0.0.0	feedback.microsoft-hohm.com >> %HOSTS%
-echo 0.0.0.0	flex.msn.com >> %HOSTS%
-echo 0.0.0.0	g.msn.com >> %HOSTS%
-echo 0.0.0.0	h1.msn.com >> %HOSTS%
-echo 0.0.0.0	lb1.www.ms.akadns.net >> %HOSTS%
-echo 0.0.0.0	live.rads.msn.com >> %HOSTS%
-echo 0.0.0.0	m.adnxs.com >> %HOSTS%
-echo 0.0.0.0	m.hotmail.com >> %HOSTS%
-echo 0.0.0.0	msedge.net >> %HOSTS%
-echo 0.0.0.0	msftncsi.com >> %HOSTS%
-echo 0.0.0.0	msnbot-65-55-108-23.search.msn.com >> %HOSTS%
-echo 0.0.0.0	msntest.serving-sys.com >> %HOSTS%
-echo 0.0.0.0	pre.footprintpredict.com >> %HOSTS%
-echo 0.0.0.0	preview.msn.com >> %HOSTS%
-echo 0.0.0.0	pricelist.skype.com >> %HOSTS%
-echo 0.0.0.0	rad.live.com >> %HOSTS%
-echo 0.0.0.0	rad.msn.com >> %HOSTS%
-echo 0.0.0.0	s.gateway.messenger.live.com >> %HOSTS%
-echo 0.0.0.0	s0.2mdn.net >> %HOSTS%
-echo 0.0.0.0	schemas.microsoft.akadns.net >> %HOSTS%
-echo 0.0.0.0	secure.adnxs.com >> %HOSTS%
-echo 0.0.0.0	secure.flashtalking.com >> %HOSTS%
-echo 0.0.0.0	sls.update.microsoft.com.akadns.net >> %HOSTS%
-echo 0.0.0.0	static.2mdn.net >> %HOSTS%
-echo 0.0.0.0	statsfe1.ws.microsoft.com >> %HOSTS%
-echo 0.0.0.0	statsfe2.update.microsoft.com.akadns.net >> %HOSTS%
-echo 0.0.0.0	statsfe2.ws.microsoft.com >> %HOSTS%
-echo 0.0.0.0	survey.watson.microsoft.com >> %HOSTS%
-echo 0.0.0.0	view.atdmt.com >> %HOSTS%
-echo 0.0.0.0	www.msftncsi.com >> %HOSTS%
-echo #END MF666 ANTI-TELEMETRY >> %HOSTS%
+(
+echo #BEGIN MF666 ANTI-TELEMETRY
+echo 0.0.0.0	choice.microsoft.com
+echo 0.0.0.0	choice.microsoft.com.nstac.net
+echo 0.0.0.0	df.telemetry.microsoft.com
+echo 0.0.0.0	oca.telemetry.microsoft.com
+echo 0.0.0.0	oca.telemetry.microsoft.com.nsatc.net
+echo 0.0.0.0	redir.metaservices.microsoft.com
+echo 0.0.0.0	reports.wes.df.telemetry.microsoft.com
+echo 0.0.0.0	services.wes.df.telemetry.microsoft.com
+echo 0.0.0.0	settings-sandbox.data.microsoft.com
+echo 0.0.0.0	settings-win.data.microsoft.com
+echo 0.0.0.0	sqm.df.telemetry.microsoft.com
+echo 0.0.0.0	sqm.telemetry.microsoft.com
+echo 0.0.0.0	sqm.telemetry.microsoft.com.nsatc.net
+echo 0.0.0.0	telecommand.telemetry.microsoft.com
+echo 0.0.0.0	telecommand.telemetry.microsoft.com.nsatc.net
+echo 0.0.0.0	telemetry.appex.bing.net
+echo 0.0.0.0	telemetry.microsoft.com
+echo 0.0.0.0	telemetry.urs.microsoft.com
+echo 0.0.0.0	vortex-sandbox.data.microsoft.com
+echo 0.0.0.0	vortex-win.data.microsoft.com
+echo 0.0.0.0	vortex.data.microsoft.com
+echo 0.0.0.0	watson.telemetry.microsoft.com
+echo 0.0.0.0	watson.telemetry.microsoft.com.nsatc.net
+echo 0.0.0.0	watson.ppe.telemetry.microsoft.com
+echo 0.0.0.0	wes.df.telemetry.microsoft.com
+echo 0.0.0.0	vortex-bn2.metron.live.com.nsatc.net
+echo 0.0.0.0	vortex-cy2.metron.live.com.nsatc.net
+echo 0.0.0.0	watson.live.com
+echo 0.0.0.0	watson.microsoft.com
+echo 0.0.0.0	feedback.search.microsoft.com
+echo 0.0.0.0	feedback.windows.com
+echo 0.0.0.0	corp.sts.microsoft.com
+echo 0.0.0.0	diagnostics.support.microsoft.com
+echo 0.0.0.0	i1.services.social.microsoft.com
+echo 0.0.0.0	i1.services.social.microsoft.com.nsatc.net
+echo 0.0.0.0	vortex-bn2.metron.live.com.nsatc.net
+echo 0.0.0.0	vortex-cy2.metron.live.com.nsatc.net
+echo 0.0.0.0	ca.telemetry.microsoft.com
+echo 0.0.0.0	cache.datamart.windows.com
+echo 0.0.0.0	spynet2.microsoft.com
+echo 0.0.0.0	spynetalt.microsoft.com
+echo 0.0.0.0	a.ads1.msn.com
+echo 0.0.0.0	a.ads2.msads.net
+echo 0.0.0.0	a.ads2.msn.com
+echo 0.0.0.0	a.rad.msn.com
+echo 0.0.0.0	a-0001.a-msedge.net
+echo 0.0.0.0	a-0002.a-msedge.net
+echo 0.0.0.0	a-0003.a-msedge.net
+echo 0.0.0.0	a-0004.a-msedge.net
+echo 0.0.0.0	a-0005.a-msedge.net
+echo 0.0.0.0	a-0006.a-msedge.net
+echo 0.0.0.0	a-0007.a-msedge.net
+echo 0.0.0.0	a-0008.a-msedge.net
+echo 0.0.0.0	a-0009.a-msedge.net
+echo 0.0.0.0	ac3.msn.com
+echo 0.0.0.0	ad.doubleclick.net
+echo 0.0.0.0	adnexus.net
+echo 0.0.0.0	adnxs.com
+echo 0.0.0.0	ads.msn.com
+echo 0.0.0.0	ads1.msads.net
+echo 0.0.0.0	ads1.msn.com
+echo 0.0.0.0	aidps.atdmt.com
+echo 0.0.0.0	aka-cdn-ns.adtech.de
+echo 0.0.0.0	a-msedge.net
+echo 0.0.0.0	apps.skype.com
+echo 0.0.0.0	az361816.vo.msecnd.net
+echo 0.0.0.0	az512334.vo.msecnd.net
+echo 0.0.0.0	b.ads1.msn.com
+echo 0.0.0.0	b.ads2.msads.net
+echo 0.0.0.0	b.rad.msn.com
+echo 0.0.0.0	bs.serving-sys.com
+echo 0.0.0.0	c.atdmt.com
+echo 0.0.0.0	c.msn.com
+echo 0.0.0.0	cdn.atdmt.com
+echo 0.0.0.0	cds26.ams9.msecn.net
+echo 0.0.0.0	compatexchange.cloudapp.net
+echo 0.0.0.0	corpext.msitadfs.glbdns2.microsoft.com
+echo 0.0.0.0	cs1.wpc.v0cdn.net
+echo 0.0.0.0	db3aqu.atdmt.com
+echo 0.0.0.0	ec.atdmt.com
+echo 0.0.0.0	fe2.update.microsoft.com.akadns.net
+echo 0.0.0.0	feedback.microsoft-hohm.com
+echo 0.0.0.0	flex.msn.com
+echo 0.0.0.0	g.msn.com
+echo 0.0.0.0	h1.msn.com
+echo 0.0.0.0	lb1.www.ms.akadns.net
+echo 0.0.0.0	live.rads.msn.com
+echo 0.0.0.0	m.adnxs.com
+echo 0.0.0.0	m.hotmail.com
+echo 0.0.0.0	msedge.net
+echo 0.0.0.0	msftncsi.com
+echo 0.0.0.0	msnbot-65-55-108-23.search.msn.com
+echo 0.0.0.0	msntest.serving-sys.com
+echo 0.0.0.0	pre.footprintpredict.com
+echo 0.0.0.0	preview.msn.com
+echo 0.0.0.0	pricelist.skype.com
+echo 0.0.0.0	rad.live.com
+echo 0.0.0.0	rad.msn.com
+echo 0.0.0.0	s.gateway.messenger.live.com
+echo 0.0.0.0	s0.2mdn.net
+echo 0.0.0.0	schemas.microsoft.akadns.net
+echo 0.0.0.0	secure.adnxs.com
+echo 0.0.0.0	secure.flashtalking.com
+echo 0.0.0.0	sls.update.microsoft.com.akadns.net
+echo 0.0.0.0	static.2mdn.net
+echo 0.0.0.0	statsfe1.ws.microsoft.com
+echo 0.0.0.0	statsfe2.update.microsoft.com.akadns.net
+echo 0.0.0.0	statsfe2.ws.microsoft.com
+echo 0.0.0.0	survey.watson.microsoft.com
+echo 0.0.0.0	view.atdmt.com
+echo 0.0.0.0	www.msftncsi.com
+echo #END MF666 ANTI-TELEMETRY
+) > %HOSTS%
 echo Blocking hosts successful!
 echo. & ping localhost -n 2 > nul
 
